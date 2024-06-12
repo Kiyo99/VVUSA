8216ba1dd20b5c2840bee88ab07f7be16584879f
kiyo99 <kiofavour@gmail.com>
2024-06-12 16:50:28 -0400

    Updated corner radius of product categories || Added screenshots

--- app/src/main/java/com/example/vvusa/CampusMarketFragment.java
+++ app/src/main/java/com/example/vvusa/CampusMarketFragment.java
@@ -10,15 +10,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CampusMarketFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class CampusMarketFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
@@ -30,14 +23,6 @@ public class CampusMarketFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CampusMarketFragment.
-     */
     // TODO: Rename and change types and number of parameters
     public static CampusMarketFragment newInstance(String param1, String param2) {
         CampusMarketFragment fragment = new CampusMarketFragment();
